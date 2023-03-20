5188d08ecebfac2fd1ad2a9d8b784867bb5b727d
Rick Boss <rickbau5@users.noreply.github.com>
2023-03-20 10:45:53 -0700

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import ListPage from './list_page'
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -11,16 +12,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-wrap bg-blue w-full h-screen">
-        <div className="h-20 w-full bg-red-50">
+      <main className="flex flex-col bg-blue">
+        <div className={`${styles.header} bg-red-50`}>
           <div>hello</div>
         </div>
-        <div className="w-1/6 min-h-full bg-red-500">
+        <div className="flex flex-row">
+          <div className={`${styles.sidebar} bg-red-500`}>
 
-        </div>
-          <div className="flex flex-col w-5/6 h-5/6 p-3 ">
+          </div>
+          <div className="w-full p-3">
             <ListPage/>
           </div>
+        </div>
       </main>
     </>
   )
